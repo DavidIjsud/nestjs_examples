4629c9f0b7a4f5e2c9e66a871f728b5505054d28
David Garcia <davidijsud@gmail.com>
2022-06-19 23:10:46 -0400

    Basic configuration of typeOrm using postgress.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import  config  from './config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+     ConfigModule.forRoot({
+        envFilePath:  process.env.NODE_ENV || '.env' ,
+        isGlobal: true,
+        load: [config],
+        validationSchema: Joi.object({
+          POSTGRES_HOST: Joi.string().required(),
+          POSTGRES_PORT: Joi.number().required(),
+          POSTGRES_USER: Joi.string().required(),
+          POSTGRES_PASSWORD: Joi.string().required(),
+          POSTGRES_DB: Joi.string().required(),
+        }),
+     }),
+     DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/database/database.module.ts
@@ -0,0 +1,26 @@
+import { Global, Module } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config  from '../config';
+
+@Global()
+@Module({
+    imports: [
+         TypeOrmModule.forRootAsync({
+              inject: [ config.KEY ],
+              useFactory: (configuration:  ConfigType<typeof config > ) => {
+                    const { host,port,username,password,database} = configuration.postgres;
+                    return {
+                        type: 'postgres',
+                        host: host,
+                        port: port,
+                        username: username,
+                        password: password,
+                        database: database,
+                };
+              },
+         }),
+    ],
+    exports: [ TypeOrmModule ]  
+})
+export class DatabaseModule {}
